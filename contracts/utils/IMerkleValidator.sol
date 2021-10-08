a64aba0dcafb5e1c028cd5fff7b356a9599b1f64
viraj124 <virajm72@gmail.com>
2021-10-08 22:13:05 +0530

    chore: update comments

--- contracts/utils/IMerkleValidator.sol
+++ contracts/utils/IMerkleValidator.sol
@@ -1,8 +1,7 @@
 pragma solidity >=0.8.4;
 
-// Allows anyone to claim a token if they exist in a merkle root.
 interface IMerkleValidator {
-    // Returns the merkle root of the merkle tree containing account balances available to claim.
+    // Returns the merkle root of the merkle tree containing user specifc calldata.
     function merkleRoot() external view returns (bytes32);
 
     // Verify users based on calldata
