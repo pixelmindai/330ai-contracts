b3f2e5a9cfab632b3bd6af6dbb6abdf655bf4283
viraj124 <virajm72@gmail.com>
2021-10-09 22:03:09 +0530

    refactor: updated the contract with complete metadata code

--- contracts/AwakeningSeries.sol
+++ contracts/AwakeningSeries.sol
@@ -4,12 +4,14 @@ pragma solidity >=0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./utils/MerkelValidator.sol";
 
-contract AwakeningSeries is ERC721, MerkelValidator {
+contract AwakeningSeries is ERC721, MerkelValidator, Ownable {
     uint256 mintDeadline = block.timestamp + 7 days;
     // using zero address for now
     address public constant treasury = 0x0000000000000000000000000000000000000000;
+    string public baseTokenURI;
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -17,7 +19,24 @@ contract AwakeningSeries is ERC721, MerkelValidator {
     /**
      * @notice Constructor which primarily used to set the base url and setting all nft's for sale
      **/
-    constructor(bytes32 _merkleRoot) ERC721("Awakening", "AWAKENING") MerkelValidator(_merkleRoot) {}
+    constructor(bytes32 _merkleRoot) ERC721("Awakening", "AWAKENING") MerkelValidator(_merkleRoot) {
+        setBaseURI("hosted_webserver_endpoint");
+    }
+
+    /**
+     * @notice Returns the current Base URI
+     */
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
+    }
+
+    /**
+     * @notice Allows owner to update the base url
+    /// @param baseURI New Base URI
+    */
+    function setBaseURI(string memory baseURI) public onlyOwner {
+        baseTokenURI = baseURI;
+    }
 
     /**
      * @notice Mints Awakening Series NFT
@@ -39,9 +58,44 @@ contract AwakeningSeries is ERC721, MerkelValidator {
      * @notice returns the tokenURI of the nft
      * @param _tokenId token id
      */
-    function tokenURI(uint256 _tokenId) public pure override returns (string memory) {
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
         require(_tokenId >= 0, "Invalid ID");
-        // since the url is not finallized so leaving it as it is right now
-        return ("https://ipfs.io/ipfs/hash");
+        return strConcat(_baseURI(), uintToStr(_tokenId));
+    }
+
+    /**
+     * @notice Concatenate two strings
+     * @param _a the first string
+     * @param _b the second string
+     * @return result the concatenation of `_a` and `_b`
+     */
+    function strConcat(string memory _a, string memory _b) internal pure returns (string memory result) {
+        result = string(abi.encodePacked(bytes(_a), bytes(_b)));
+    }
+
+    /**
+     * @notice Convert a `uint` value to a `string`
+     * via OraclizeAPI - MIT licence
+     * https://github.com/provable-things/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol#L896
+     * @param _i the `uint` value to be converted
+     * @return result the `string` representation of the given `uint` value
+     */
+    function uintToStr(uint256 _i) internal pure returns (string memory result) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint256 j = _i;
+        uint256 len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint256 k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = bytes1(uint8(48 + (_i % 10)));
+            _i /= 10;
+        }
+        result = string(bstr);
     }
 }
