854009d37f17858e06d15ab1701a680d51ee2823
viraj124 <virajm72@gmail.com>
2021-10-12 18:06:07 +0530

    feat: test coverage for awakening series contract

--- contracts/AwakeningSeries.sol
+++ contracts/AwakeningSeries.sol
@@ -4,9 +4,11 @@ pragma solidity >=0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "./utils/MerkelValidator.sol";
 
 contract AwakeningSeries is ERC721, MerkelValidator, Ownable {
+    using Strings for uint256;
     uint256 private mintDeadline;
     string public baseTokenURI;
 
@@ -44,12 +46,17 @@ contract AwakeningSeries is ERC721, MerkelValidator, Ownable {
      * @notice Mints Awakening Series NFT
      /// @param index Merkle Proof User Index
      /// @param merkleProof Merkle Proof User Proof Array
+     /// @param _data CallData of function
      /// These details will be available when merkel root is generated
      * @return _tokenId token id
      */
-    function mint(uint256 index, bytes32[] calldata merkleProof) external returns (uint256) {
+    function mint(
+        uint256 index,
+        bytes32[] calldata merkleProof,
+        bytes calldata _data
+    ) external returns (uint256) {
         require(block.timestamp < mintDeadline, "AwakeningSeries: Minting Over");
-        verify(index, msg.data, true, merkleProof);
+        verify(index, _data, true, merkleProof);
         uint256 id = _tokenIds.current();
         _tokenIds.increment();
         _mint(msg.sender, id);
@@ -61,7 +68,7 @@ contract AwakeningSeries is ERC721, MerkelValidator, Ownable {
      * @param _tokenId token id
      */
     function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-        return strConcat(_baseURI(), uintToStr(_tokenId));
+        return strConcat(_baseURI(), Strings.toString(_tokenId));
     }
 
     /**
@@ -72,31 +79,5 @@ contract AwakeningSeries is ERC721, MerkelValidator, Ownable {
      */
     function strConcat(string memory _a, string memory _b) internal pure returns (string memory result) {
         result = string(abi.encodePacked(bytes(_a), bytes(_b)));
-    }
-
-    /**
-     * @notice Convert a `uint` value to a `string`
-     * via OraclizeAPI - MIT licence
-     * https://github.com/provable-things/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol#L896
-     * @param _i the `uint` value to be converted
-     * @return result the `string` representation of the given `uint` value
-     */
-    function uintToStr(uint256 _i) internal pure returns (string memory result) {
-        if (_i == 0) {
-            return "0";
-        }
-        uint256 j = _i;
-        uint256 len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint256 k = len - 1;
-        while (_i != 0) {
-            bstr[k--] = bytes1(uint8(48 + (_i % 10)));
-            _i /= 10;
-        }
-        result = string(bstr);
     }
 }
---
+++ test/awakening.series.test.ts
@@ -0,0 +1,114 @@
+import { ethers } from "hardhat";
+import chai, { assert } from "chai";
+import { solidity } from "ethereum-waffle";
+import { AwakeningSeries__factory, AwakeningSeries } from "../typechain";
+
+chai.use(solidity);
+const { expect } = chai;
+
+describe("Awakening Series", () => {
+  let instance: AwakeningSeries;
+
+  const user1Data =
+    "0x54d3cf5d0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002c427079774443635573446e5562646a4741675534697636634232327a6a32596a3238456653685a70726a78760000000000000000000000000000000000000000";
+
+  const user2Data =
+    "0x54d3cf5d0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002c427079774443635573446e5562646a4741675534697636634232327a6a32596a3238456653685a70726a78760000000000000000000000000000000000000001";
+
+  const invalidData =
+    "0x54d3cf5d4000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002c427079774443635573446e5562646a4741675534697636634232327a6a32596a3238456653685a70726a78760000000000000000000000000000000000000001";
+
+  // 100 mins
+  const mintDeadline = 1634047750;
+
+  // sample root for testing
+  const merkelRoot = "0xafde3f7e67c99e3b964fc486d9aa242c8f7cc7a614f239b5328d7c4396a966d5";
+
+  // sample config for testing
+  const proofConfig: any[] = [
+    // valid proofs
+    {
+      "0x54d3cf5d0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002c427079774443635573446e5562646a4741675534697636634232327a6a32596a3238456653685a70726a78760000000000000000000000000000000000000000":
+        {
+          index: 0,
+          proof: ["0x7f4c2c044c8d20fa27564cf703ef6e7ac917313ff9e83ba03a0d74c62d10ec38"],
+        },
+    },
+    {
+      "0x54d3cf5d0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002c427079774443635573446e5562646a4741675534697636634232327a6a32596a3238456653685a70726a78760000000000000000000000000000000000000001":
+        {
+          index: 1,
+          proof: ["0x63ce5ddcce1cb24eecbb86694a801de410da39e91694bc7c0df19453c4863abf"],
+        },
+    },
+    // invalid proof
+    {
+      "0x54d3cf5d4000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002c427079774443635573446e5562646a4741675534697636634232327a6a32596a3238456653685a70726a78760000000000000000000000000000000000000001":
+        {
+          index: 3,
+          proof: ["0x63ce5ddcde1cb24eecbb86694a801de410da39e91694bc7c0df19453c4863abf"],
+        },
+    },
+  ];
+
+  beforeEach(async () => {
+    const [deployer, user] = await ethers.getSigners();
+
+    const seriesDeployer = new AwakeningSeries__factory(deployer);
+    instance = await seriesDeployer.deploy(merkelRoot, mintDeadline, "https://meta.330.ai/01/");
+  });
+
+  describe("should allow users to mint", async () => {
+    it("makes sure the 1st verified account can mint", async () => {
+      const [deployer] = await ethers.getSigners();
+      await instance.mint(proofConfig[0][user1Data].index, proofConfig[0][user1Data].proof, user1Data);
+      const owner = await instance.ownerOf(0);
+      assert(owner === deployer.address);
+      const tokenUrl = await instance.tokenURI(0);
+      assert(tokenUrl == "https://meta.330.ai/01/0");
+    });
+
+    it("makes sure the 2nd verified account can mint", async () => {
+      const [, user] = await ethers.getSigners();
+      await instance.mint(proofConfig[0][user1Data].index, proofConfig[0][user1Data].proof, user1Data);
+      await instance.connect(user).mint(proofConfig[1][user2Data].index, proofConfig[1][user2Data].proof, user2Data);
+      const owner = await instance.ownerOf(1);
+      assert(owner === user.address);
+      const tokenUrl = await instance.tokenURI(1);
+      assert(tokenUrl == "https://meta.330.ai/01/1");
+    });
+
+    it("reverts if invalid proof is provided", async () => {
+      await expect(
+        instance.mint(proofConfig[0][user1Data].index, proofConfig[2][invalidData].proof, invalidData),
+      ).to.be.revertedWith("MerkelValidator: Invalid proof");
+    });
+
+    it("reverts if mint deadline is passed", async () => {
+      const blockNum = await ethers.provider.getBlockNumber();
+      const block = await ethers.provider.getBlock(blockNum);
+      const currentTimestamp = block.timestamp;
+      for (let i = 0; i < mintDeadline - currentTimestamp; i++) {
+        await ethers.provider.send("evm_mine", []);
+      }
+      await expect(
+        instance.mint(proofConfig[0][user1Data].index, proofConfig[0][user1Data].proof, user1Data),
+      ).to.be.revertedWith("AwakeningSeries: Minting Over");
+    });
+  });
+
+  describe("owner should be able to perform metadata updates", async () => {
+    it("reverts if a non-owner tries to update the token base uri", async () => {
+      const [, user] = await ethers.getSigners();
+      await expect(instance.connect(user).setBaseURI("https://beta.330.ai/01/")).to.be.revertedWith(
+        "Ownable: caller is not the owner",
+      );
+    });
+
+    it("allows owner to update the token base uri", async () => {
+      await instance.setBaseURI("https://beta.330.ai/01/");
+      const baseUri = await instance.baseTokenURI();
+      assert(baseUri == "https://beta.330.ai/01/");
+    });
+  });
+});
