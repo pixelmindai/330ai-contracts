7cc90d3400f68ce72d14fa4ca65e42f4e6730414
Karim Boucher <boucherkarim@gmail.com>
2021-12-13 12:32:01 -0500

    Use `ERC721URIStorage`

--- contracts/CollectorPassG1.sol
+++ contracts/CollectorPassG1.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract CollectorPassG1 is ERC721, Ownable {
+contract CollectorPassG1 is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
     uint public MAXSUPPLY;
